0291bb810fc3b2f2ab8c5ed9d9ef9feb99c9f94f
Yero-des <yero_zc@hotmail.com>
2023-10-27 14:43:25 -0500

    Add group styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,81 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import { Cantidatos } from './components/cantidatos';
+import "./css/App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Cantidatos } from "./components/cantidatos";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
 
+  const [height, setHeight] = useState();
+  
+  useEffect(() => {
+    
+    const handleResize = () => {               
+  
+      const navHeight = document.getElementById('nav-bar').offsetHeight;  
+      setHeight(window.innerHeight - navHeight); 
+
+    }
+
+    const navHeight = document.getElementById('nav-bar').offsetHeight;   
+    setHeight(window.innerHeight - navHeight);  
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
+
   const cantidatos = [
     {
-      name: 'Lista 1',
-      img: "lista1.png",
+      nombre: "Lista 1",
+      descripcion: "Esta es una pequeña descripción del grupo general",
+      alcalde: "Jorge",
+      img: "/src/img/foto2.jpg",
+      simbolo: "/src/assets/insignia.png",
       votos: "0",
     },
     {
-      name: 'Lista 2',
-      img: "lista2.png",
+      nombre: "Lista 2",
+      descripcion: "Esta es una pequeña descripción del grupo general",
+      alcalde: "Juan",
+      img: "/src/img/foto3.jpg",
+      simbolo: "/src/assets/insignia.png",
       votos: "0",
     },
     {
-      name: 'Lista 3',
-      img: "lista3.png",
-      votos: "0",
-    },
-    {
-      name: 'Lista 4',
-      img: "lista3.png",
-      votos: "0",
-    },
-    {
-      name: 'Lista 5',
-      img: "lista3.png",
-      votos: "0",
-    },
-    {
-      name: 'Lista 6',
-      img: "lista3.png",
+      nombre: "Lista 3",
+      descripcion: "Esta es una pequeña descripción del grupo general",
+      alcalde: "Juan",
+      img: "/src/img/foto4.jpg",
+      simbolo: "/src/assets/insignia.png",
       votos: "0",
     }
   ];
 
   return (
     <>
-      <div className='container'>
-        <h1>ELECCIONES MUNICIPIO ESCOLAR 2023</h1>
-        <h2>INSTITUCIÓN EDUCATIVA N° 2080 ANDRES BELLO</h2>
+      <nav id="nav-bar" className="navbar navbar-expand-lg bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand text-uppercase text-light" href="#">
+            <div className="d-flex align-items-center">
+              {/* <img src="/src/assets/insignia.png" alt="asdf" width={30} height={50}/>               */}
+              Elecciones municipio escolar 2023
+            </div>
+            </a>
+        </div>
+      </nav>
+      <div className="font text-center" style={{height: height}}>
+        <div className="gray-content-font"></div>
+        <div className="content-font">     
+          <Cantidatos cantidatos={cantidatos} />
+        </div>
       </div>
-      <Cantidatos cantidatos={cantidatos}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Cantidatos.jsx
+++ src/components/Cantidatos.jsx
@@ -1,19 +1,35 @@
-import '../public/css/cantidatos.css'
+import "../css/Cantidatos.css";
 
-export const Cantidatos = ({cantidatos}) => {
+export const Cantidatos = ({ cantidatos }) => {
   return (
     <div className="cantidatos mt-4">
-
       {cantidatos.map((value, index) => {
         return (
-          <article className="cantidato">
-            <img src={value.img} alt={value.name} />
-            <h2>{value.name}</h2>
-            <button className='btn btn-success'>Votar</button>
-          </article>
-        )
+          <div href="#" key={index} className="cantidato">
+            <div className="group-img" title={value.descripcion}>
+              <img src={value.img} alt={value.nombre} />
+              <div className="group-title">
+                <h3 className="text-light text-center">{value.nombre}</h3>
+              </div>
+              <div className="group-simbol">
+                <img
+                  src={value.simbolo}
+                  alt={value.nombre}
+                  width={30}
+                  height={60}
+                />
+              </div>
+            </div>
+            <div className="group-content container">
+              <div className="row">
+                <button className="col btn btn-success btn-vote">
+                  <i class="bi bi-check2-circle text-light"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        );
       })}
-
     </div>
-  )
-}
+  );
+};
